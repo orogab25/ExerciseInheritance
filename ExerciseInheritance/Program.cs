47550e3f7b2b44454d9020da0e5dfdaa494fac8c
orogab25 <orogab25@gmail.com>
2020-03-24 13:42:17 +0100

    Add project files.

---
+++ ExerciseInheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExerciseInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ ExerciseInheritance/Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseInheritance
+{
+    class Stack
+    {
+        private readonly Stack<object> stack = new Stack<object>();
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                stack.Push(obj);
+            }
+            else
+            {
+                throw new InvalidOperationException("obj is null");
+            }
+        }
+
+        public object Pop()
+        {
+            if (stack == null || stack.Count==0)
+            {
+                throw new InvalidOperationException("stack is empty");
+            }
+            return stack.Pop();
+        }
+
+        public void Clear()
+        {
+            stack.Clear();
+        }
+    }
+}
